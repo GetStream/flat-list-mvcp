31700cee598c9cc2e60e445099a48708cc6c1228
vishtree <vishalnarkhede.iitd@gmail.com>
2021-02-20 18:59:23 +0100

    Adding ts declarations

--- Example/App.tsx
+++ Example/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 // eslint-disable-next-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import ScrollViewExample from './src/ScrollViewExample';
 import FlatListExample from './src/FlatListExample';
 
